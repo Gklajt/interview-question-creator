0efa9cc52aa23bcc7692ded5e86c895c1c99f7bc
gokul <gkl.ajt@gmail.com>
2024-06-08 20:36:25 +0530

    project template added

---
+++ setup.py
@@ -0,0 +1,12 @@
+# creating a local package
+from setuptools import find_packages, setup
+
+setup(
+
+    name = "Generative AI Project",
+    version ='0.0.0',
+    author = "Gokul A",
+    author_email ="gkl.ajt@gmail.com",
+    packages = find_packages(),
+    install_required=[]
+)
